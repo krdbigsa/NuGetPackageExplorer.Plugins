267c528f7e6fb479997af2ba9aa6d5a54a6a699a
Lucas Kaczanowski <lucas.kaczanowski@gmail.com>
2014-05-24 15:19:58 +0200

    Added new project with partially implemented delete package command

---
+++ src/NuGetPackageExplorer.Plugins.DeleteCommand/Controls/SettingsControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using NuGet;
+
+namespace NuGetPackageExplorer.Plugins.DeleteCommand.Controls
+{
+  /// <summary>
+  /// Interaction logic for SettingsControl.xaml
+  /// </summary>
+  public partial class SettingsControl : UserControl
+  {
+    private readonly IPackage _package;
+
+    private readonly string _packagePath;
+
+    public SettingsControl()
+    {
+      InitializeComponent();
+    }
+
+    public SettingsControl(IPackage package, string packagePath)
+      : this()
+    {
+      _package = package;
+      _packagePath = packagePath;
+
+      // TODO: add some proper binding
+
+      QuestionLabel.Content = string.Format("Are you sure you want to remove package [{0}, {1}] from source?", _package.Id, _package.Version);
+
+      SourceText.Text = GetPackageSource(_packagePath);
+
+      ApiKeyText.Text = _package.Id;
+    }
+
+    public Window ParentWindow
+    {
+      get
+      {
+        return Parent as Window;
+      }
+    }
+
+    private string GetPackageSource(string packagePath)
+    {
+      // TODO: parse source url from path
+      return packagePath;
+    }
+
+    private void OnButtonOkClick(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        // TODO: execute delete command
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+    }
+
+    private void OnButtonCancelClick(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        if (ParentWindow != null)
+        {
+          ParentWindow.Close();
+        }
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+    }
+  }
+}
---
+++ src/NuGetPackageExplorer.Plugins.DeleteCommand/DeletePackageCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+using NuGet;
+
+using NuGetPackageExplorer.Plugins.DeleteCommand.Controls;
+using NuGetPackageExplorer.Types;
+
+namespace NuGetPackageExplorer.Plugins.DeleteCommand
+{
+  [PackageCommandMetadata("Delete selected package...")]
+  internal class DeletePackageCommand : IPackageCommand
+  {
+    public void Execute(IPackage package, string packagePath)
+    {
+      try
+      {
+        var settingsDialog = new Window
+        {
+          Content = new SettingsControl(package, packagePath),
+          Topmost = true,
+          Width = 600,
+          Height = 200,
+          ResizeMode = ResizeMode.NoResize,
+          WindowStartupLocation = WindowStartupLocation.CenterOwner,
+          ShowInTaskbar = false
+        };
+
+        settingsDialog.ShowDialog();
+
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+    }
+  }
+}
